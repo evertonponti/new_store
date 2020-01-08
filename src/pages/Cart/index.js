4f9d8beaff411b4485dcfbfee7913c1d9e766d09
Everton Ponti <everton.ponti@gmail.com>
2020-01-08 17:41:21 -0300

    Layout Estatico da Tela de Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Meu Carrinho</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTDE.</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://images1.kabum.com.br/produtos/fotos/94211/94211_1514308386_index_gg.jpg"
+                alt="Placa-Mãe Gigabyte Z370M Aorus Gaming, Intel LGA 1151, mATX, DDR4"
+              />
+            </td>
+            <td>
+              <strong>Placa-Mãe Gigabyte Z370M Aorus Gaming, Intel LGA 1151, mATX, DDR4</strong>
+              <span>R$677,67</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#5a2d82" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#5a2d82" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="5a2d82" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$259,80</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
