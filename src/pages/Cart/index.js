1a672731614aa4f4be54e2d0e8f6c32f022ec247
Everton Ponti <everton.ponti@gmail.com>
2020-01-09 14:25:50 -0300

    Configurando telas com o Reducer para controle de estado (adicionar produto no carrinho)

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -7,7 +9,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +23,36 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://images1.kabum.com.br/produtos/fotos/94211/94211_1514308386_index_gg.jpg"
-                alt="Placa-Mãe Gigabyte Z370M Aorus Gaming, Intel LGA 1151, mATX, DDR4"
-              />
-            </td>
-            <td>
-              <strong>Placa-Mãe Gigabyte Z370M Aorus Gaming, Intel LGA 1151, mATX, DDR4</strong>
-              <span>R$677,67</span>
-            </td>
-            <td>
-              <div>
+        {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#d35400" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#d35400" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#d35400" />
+                  <MdDelete size={20} color="5a2d82" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#d35400" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$677,67</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="5a2d82" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -66,3 +67,9 @@ export default function Cart() {
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
